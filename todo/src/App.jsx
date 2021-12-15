7ab04bfa5d75350ddb02448e51eafe0bb587b95f
ulldecorb <ausias.bertran@gmail.com>
2021-12-15 12:09:35 +0100

    Connect components by props

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -3,7 +3,14 @@ import { TodoList } from './components/TodoList';
 import List from './constants/List';
 
 export const App = function App() {
+  // const [todos, setTodos] = useState([...List]);
   const todos = [...List];
+
+  // function deleteTask(id) {
+  //   const newtodos = todos.filter((task) => task.id !== id);
+  //   setTodos(newtodos);
+  // }
+
   return (
     <section>
       <TodoList todos={todos} />
