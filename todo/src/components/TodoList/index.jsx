52f6162db74cc39afd8674485c150896d8dde593
ulldecorb <ausias.bertran@gmail.com>
2021-12-17 13:20:05 +0100

    Create handdleDeleteTask function and connect by props

--- todo/src/components/TodoList/index.jsx
+++ todo/src/components/TodoList/index.jsx
@@ -1,22 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { TodoItem } from '../TodoItem/index';
+import { TodoItem } from '../TodoItem';
 
-export const TodoList = function TodoList({ todos }) {
+export const TodoList = function TodoList({ todos, handleDeleteTask }) {
   return (
     <>
       <h1>TODO LIST</h1>
       <ul>
         {todos.map((todo) => (
-          <TodoItem todo={todo} />
+          <li>
+            <TodoItem data={todo} handleDeleteTask={handleDeleteTask} />
+            {todo.task}
+
+          </li>
         ))}
       </ul>
+      <div>
+        <input type="text" name="addtask" id="addtask" />
+      </div>
     </>
   );
 };
 
 TodoList.propTypes = {
-  todos: PropTypes.shape([]).isRequired
+  todos: PropTypes.shape([]).isRequired,
+  handleDeleteTask: PropTypes.func.isRequired
 };
 
 export default TodoList;
